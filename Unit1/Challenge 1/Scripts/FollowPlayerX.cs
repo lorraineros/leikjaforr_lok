55b1225d5196b4abe031af1590f4582cc2ea6f43
lorraineros <43746757+lorraineros@users.noreply.github.com>
2020-05-06 11:26:25 +0000

    done :)

---
+++ Unit1/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    [SerializeField] private GameObject plane;
+    [SerializeField] private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // chasing cam
+        /*
+        Vector3 rotOffset = plane.transform.rotation * offset;
+        transform.position = plane.transform.position + rotOffset;
+        transform.rotation = plane.transform.rotation;
+        */
+
+        transform.position = plane.transform.position + offset;
+
+
+    }
+}
